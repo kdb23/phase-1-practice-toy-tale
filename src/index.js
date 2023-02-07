086cbfaeafe1777cae784efb1ae3d93b4021f43b
Samantha Genevay <samanthagenevay@gmail.com>
2023-02-07 10:15:14 -0500

    created more toy variables

--- src/index.js
+++ src/index.js
@@ -18,3 +18,16 @@ const toyInfo = document.querySelector('#toy-collection');
 toyCard = document.createElement("div");
 toyCard.className = "card"
 toyInfo.append(toyCard)
+
+
+const toyName = document.createElement("h2");
+const toyImage = document.createElement("img");
+toyImage.className = "toy-avatar";
+// toyImage.src = "image-attribute"; // go back to this one
+const toyLikes = document.createElement("p");
+const button = document.createElement("button");
+button.className = "like-btn";
+  // button.id = 
+
+toyCard.append(toyName, toyImage, toyLikes, button);
+
