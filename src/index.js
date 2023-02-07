83b9e4b3fea4288a1f8957c90963d68d7792a0d7
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-02-07 11:11:27 -0500

    fetch

--- src/index.js
+++ src/index.js
@@ -14,20 +14,30 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-const toyInfo = document.querySelector('#toy-collection');
-toyCard = document.createElement("div");
-toyCard.className = "card"
-toyInfo.append(toyCard)
+fetch('http://localhost:3000/toys')
+  .then((resp) => resp.json())
+  .then((toyObjects) => {
+    toyObjects.forEach((toy) => {
+      const toyInfo = document.querySelector('#toy-collection');
+      toyCard = document.createElement("div");
+      toyCard.className = "card"
+      toyInfo.append(toyCard)
+
+      const toyName = document.createElement("h2");
+      toyName.innerText = toy.name
+      const toyImage = document.createElement("img");
+      //img src and url
+      toyImage.className = "toy-avatar";
+      const toyLikes = document.createElement("p");
+      toyLikes.innerText = toy.likes + " " + "likes"
+      const button = document.createElement("button");
+      button.className = "like-btn";
+      
+      
+
+      toyCard.append(toyName, toyImage, toyLikes, button);
+    })
+  })
 
 
-const toyName = document.createElement("h2");
-const toyImage = document.createElement("img");
-toyImage.className = "toy-avatar";
-// toyImage.src = "image-attribute"; // go back to this one
-const toyLikes = document.createElement("p");
-const button = document.createElement("button");
-button.className = "like-btn";
-  // button.id = 
-
-toyCard.append(toyName, toyImage, toyLikes, button);
 
